c1a04f481a088b71a9eeb68a1f6dfb004706c964
Azderica <mh97888@gmail.com>
2021-01-12 22:51:00 +0900

    init project

---
+++ project1/project/readme.md
@@ -0,0 +1,6 @@
+# Project 1. 쇼핑몰.
+
+## ERD 설계
+
+![image-20210112223946548](/Users/myepark/Library/Application Support/typora-user-images/image-20210112223946548.png)
+
---
+++ project1/project/src/test/java/com/example/myepark/MyeparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myepark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyeparkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
