cc841df8105deb83eddbbf5a29d2ddce93520e23
Azderica <mh97888@gmail.com>
2021-01-19 00:17:01 +0900

    finish all api service, controller

--- project1/project/src/main/java/com/myepark/study/service/PartnerApiLogicService.java
+++ project1/project/src/main/java/com/myepark/study/service/PartnerApiLogicService.java
@@ -1,30 +1,104 @@
 package com.myepark.study.service;
 
 import com.myepark.study.ifs.CrudInterface;
+import com.myepark.study.model.entity.Category;
+import com.myepark.study.model.entity.Partner;
 import com.myepark.study.model.network.Header;
 import com.myepark.study.model.network.request.PartnerApiRequest;
 import com.myepark.study.model.network.response.PartnerApiResponse;
+import com.myepark.study.repository.CategoryRepository;
+import com.myepark.study.repository.PartnerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class PartnerApiLogicService implements CrudInterface<PartnerApiRequest, PartnerApiResponse> {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private PartnerRepository partnerRepository;
+
     @Override
     public Header<PartnerApiResponse> create(Header<PartnerApiRequest> request) {
-        return null;
+        PartnerApiRequest body = request.getData();
+
+        Partner partner = Partner.builder()
+                .name(body.getName())
+                .status(body.getStatus())
+                .address(body.getAddress())
+                .callCenter(body.getCallCenter())
+                .partnerNumber(body.getPartnerNumber())
+                .businessNumber(body.getBusinessNumber())
+                .ceoName(body.getCeoName())
+                .registeredAt(LocalDateTime.now())
+                .unregisteredAt(null)
+                .category(categoryRepository.getOne(body.getCategoryId()))
+                .build();
+
+        Partner newPartner = partnerRepository.save(partner);
+        return response(newPartner);
     }
 
     @Override
     public Header<PartnerApiResponse> read(Long id) {
-        return null;
+        return partnerRepository.findById(id)
+                .map(this::response)
+                .orElseGet(() -> Header.ERROR("No Data"));
     }
 
     @Override
     public Header<PartnerApiResponse> update(Header<PartnerApiRequest> request) {
-        return null;
+        PartnerApiRequest body = request.getData();
+
+        return partnerRepository.findById(body.getId())
+                .map(partner -> {
+                    partner
+                            .setName(body.getName())
+                            .setStatus(body.getStatus())
+                            .setAddress(body.getAddress())
+                            .setCallCenter(body.getCallCenter())
+                            .setPartnerNumber(body.getPartnerNumber())
+                            .setBusinessNumber(body.getBusinessNumber())
+                            .setCeoName(body.getCeoName())
+                            .setRegisteredAt(body.getRegisteredAt())
+                            .setUnregisteredAt(body.getUnregisteredAt())
+                            .setCategory(categoryRepository.getOne(body.getCategoryId()));
+                    return partner;
+                })
+                .map(partner -> partnerRepository.save(partner))
+                .map(this::response)
+                .orElseGet(() -> Header.ERROR("No Data"));
     }
 
     @Override
     public Header delete(Long id) {
-        return null;
+        return partnerRepository.findById(id)
+                .map(partner -> {
+                    partnerRepository.delete(partner);
+                    return Header.OK();
+                })
+                .orElseGet(() -> Header.ERROR("No Data"));
+    }
+
+    private Header<PartnerApiResponse> response(Partner partner) {
+        PartnerApiResponse body = PartnerApiResponse.builder()
+                .id(partner.getId())
+                .name(partner.getName())
+                .status(partner.getStatus())
+                .address(partner.getAddress())
+                .callCenter(partner.getCallCenter())
+                .partnerNumber(partner.getPartnerNumber())
+                .businessNumber(partner.getBusinessNumber())
+                .ceoName(partner.getCeoName())
+                .registeredAt(partner.getRegisteredAt())
+                .unregisteredAt(partner.getUnregisteredAt())
+                .categoryId(partner.getCategory().getId())
+                .build();
+
+        return Header.OK(body);
     }
 }
