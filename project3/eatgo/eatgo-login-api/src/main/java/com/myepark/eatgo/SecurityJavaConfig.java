5f7e986671dae241eb7c0e8687936a848a4a63ac
Azderica <mh97888@gmail.com>
2021-02-04 23:55:11 +0900

    all test is passed, but run is error because of hibernates

--- project3/eatgo/eatgo-login-api/src/main/java/com/myepark/eatgo/SecurityJavaConfig.java
+++ project3/eatgo/eatgo-login-api/src/main/java/com/myepark/eatgo/SecurityJavaConfig.java
@@ -23,20 +23,12 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        Filter filter = new JwtAuthenticationFilter(
-                authenticationManager(), jwtUtil());
-
         http
                 .cors().disable()
                 .csrf().disable()
                 .formLogin().disable()
-                .headers().frameOptions().disable()
-                .and()
-                .addFilter(filter)
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .headers().frameOptions().disable();
     }
-
 
     @Bean
     public PasswordEncoder passwordEncoder() {
