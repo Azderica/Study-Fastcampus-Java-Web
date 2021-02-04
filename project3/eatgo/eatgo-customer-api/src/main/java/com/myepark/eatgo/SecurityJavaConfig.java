0fd8942519ecf5cdca22c3bb02aa94f7c08000cd
Azderica <mh97888@gmail.com>
2021-02-04 23:45:36 +0900

    some error in jwt

--- project3/eatgo/eatgo-customer-api/src/main/java/com/myepark/eatgo/SecurityJavaConfig.java
+++ project3/eatgo/eatgo-customer-api/src/main/java/com/myepark/eatgo/SecurityJavaConfig.java
@@ -1,5 +1,8 @@
 package com.myepark.eatgo;
 
+import com.myepark.eatgo.filters.JwtAuthenticationFilter;
+import com.myepark.eatgo.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,14 +12,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.servlet.Filter;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("${jwt.secret}")
+    private String secret;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        Filter filter = new JwtAuthenticationFilter(
-//                authenticationManager(), jwtUtil());
+        Filter filter = new JwtAuthenticationFilter(
+                authenticationManager(), jwtUtil());
 
         http
                 .cors().disable()
@@ -24,15 +32,19 @@ public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
                 .formLogin().disable()
                 .headers().frameOptions().disable()
                 .and()
-//                .addFilter(filter)
+                .addFilter(filter)
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtUtil jwtUtil() {
+        return new JwtUtil(secret);
     }
 
 }
