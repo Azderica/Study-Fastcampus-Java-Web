89d304af4168db7fa4bb08eacbeb9137d5814bae
Azderica <mh97888@gmail.com>
2021-02-08 20:58:42 +0900

    add sample code

---
+++ PoiSample/src/main/java/com/myepark/ExcelData.java
@@ -0,0 +1,16 @@
+package com.myepark;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExcelData {
+
+    private Integer num;
+
+    private String name;
+
+    private String email;
+
+}
